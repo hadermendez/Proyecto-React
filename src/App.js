f8d5e29b7d34082994dd40e0945c964c82a5987f
Hader-personal <hrmendezg@gmail.com>
2022-11-18 21:21:42 -0500

    consuminendo y renderizando datos desde la api

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import logo from './logo.svg';
 import imagenHome from "./img/rick-morty.png";
+import Characters from "./components/Characters";
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [characters, setCharacters] = useState(null);
 
 const reqApi = async () => {
-
-  console.log("click");
+  const api = await fetch("https://rickandmortyapi.com/api/character");
+  const characterApi = await api.json();
+  setCharacters(characterApi.results);
 };
 
   return (
     <div className="App">
       <header className="App-header">
       <h1 className="title">Rick & Morty</h1>
-      <img src={imagenHome} alt="rick and morty" className="img-home"/>
+     {characters ? (
+      <Characters characters = {characters} setCharacters = {setCharacters} />
+     ):(
+      <>
+        <img src={imagenHome} alt="rick and morty" className="img-home"/>
       <button onClick={reqApi} className="btn-search" >Buscar personajes</button>
+      </>
+     )}
+     
+    
+     
       </header>
     </div>
   );
